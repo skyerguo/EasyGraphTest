12039c00b570fc11675db838a70538fec9b0ef6d
gtc <gtc@node0.easygraph.socnet-pg0.wisc.cloudlab.us>
2023-03-13 12:14:41 +0800

    first commit

---
+++ clustering_coefficiency_parellel.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+double clustering_coefficient(vector<vector<int>>& adj_list, int num_nodes) {
+    double coeff_sum = 0;
+    #pragma omp parallel for reduction(+:coeff_sum)
+    for (int i = 0; i < num_nodes; i++) {
+        int num_neighbors = adj_list[i].size();
+        if (num_neighbors < 2) continue;
+        int num_links = 0;
+        for (int j = 0; j < num_neighbors; j++) {
+            int neighbor = adj_list[i][j];
+            for (int k = j+1; k < num_neighbors; k++) {
+                if (find(adj_list[neighbor].begin(), adj_list[neighbor].end(), adj_list[i][k]) != adj_list[neighbor].end())
+                    num_links++;
+            }
+        }
+        coeff_sum += (double)num_links / ((double)num_neighbors * (double)(num_neighbors - 1));
+    }
+    return coeff_sum / num_nodes;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " graph_file" << endl;
+        return 1;
+    }
+    string graph_file = argv[1];
+
+    // Read graph from file
+    ifstream infile(graph_file);
+    int num_nodes, num_edges;
+    infile >> num_nodes >> num_edges;
+    vector<vector<int>> adj_list(num_nodes);
+    for (int i = 0; i < num_edges; i++) {
+        int node1, node2;
+        infile >> node1 >> node2;
+        adj_list[node1].push_back(node2);
+        adj_list[node2].push_back(node1);
+    }
+    infile.close();
+
+    double start_time = omp_get_wtime();
+    double coeff = clustering_coefficient(adj_list, num_nodes);
+    double end_time = omp_get_wtime();
+    cout << "Clustering Coefficient: " << coeff << endl;
+    cout << "Elapsed time: " << end_time - start_time << " seconds" << endl;
+
+    return 0;
+}
---
+++ shortest_path_dijkstra.cpp
@@ -0,0 +1,96 @@
+#include <iostream> 
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <vector>
+#include<ext/pb_ds/priority_queue.hpp>
+
+const int Maxn = 200005;
+const int Maxm = 2000005;
+
+using namespace std;
+
+int N, M;
+int head[Maxn], dis[Maxn];
+bool vis[Maxn];
+
+struct Edge_weighted{
+    int toward, next, weight;
+};
+Edge_weighted E[Maxm];
+
+struct compare_node {
+	compare_node(){} 
+    compare_node(int _x, int _d) {x = _x; d = _d;}
+	int x, d;	
+	bool operator < (const compare_node &rhs) const {
+		return d > rhs.d;
+	}
+};
+
+int edge_number = 0;
+bool if_undirected = false;
+inline void add_edge(const int &u, const int &v, const int &w) {
+    E[++edge_number].next = head[u];
+    E[edge_number].toward = v;
+    E[edge_number].weight = w;
+    head[u] = edge_number;
+}
+#define getE(p,u) for(register int p = head[u]; p; p = E[p].next)
+
+void input() {
+    FILE* input_file = fopen("../dataset/a.in", "r");
+    fscanf(input_file, " %d %d", &N, &M);
+    // scanf(" %d %d %d", &N, &M, &S);
+    int u, v, w;
+    for (int i = 0; i < M; ++i) {
+        fscanf(input_file, " %d %d %d", &u, &v, &w);
+        // scanf(" %d %d %d", &u, &v, &w);
+        add_edge(u, v, w);
+        if (if_undirected)
+            add_edge(v, u, w);
+    }
+}
+
+__gnu_pbds ::priority_queue<compare_node> q;
+void dijkstra(const int &S) {
+    memset(dis, 0x7f, sizeof(int) * N);
+    memset(vis, false, sizeof(bool) * N);
+    while (!q.empty()) q.pop();
+
+    dis[S] = 0; 
+    q.push(compare_node(S, 0));
+
+    while(!q.empty()) {
+        int u=q.top().x;
+        q.pop();
+        if (vis[u]) 
+            continue;
+        vis[u] = true;
+
+        for(register int p = head[u]; p; p = E[p].next) {
+            int v = E[p].toward;
+            if (dis[v] > dis[u] + E[p].weight) {
+                dis[v] = dis[u] + E[p].weight;
+                q.push(compare_node(v, dis[v]));
+            }
+        }
+    }
+}
+
+FILE* output_file = fopen("./results/a.out", "w");
+void output() {
+    for (int j = 0; j < N; ++j)
+        fprintf(output_file, "%d\n", dis[j]);
+    fprintf(output_file, "\n");
+}
+
+int main() {
+    input();
+    for (int i = 0; i < N; ++i) {
+        dijkstra(i);
+        // output();
+    }
+    return 0;
+}
