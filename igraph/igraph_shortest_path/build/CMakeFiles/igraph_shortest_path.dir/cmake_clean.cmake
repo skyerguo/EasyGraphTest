da138630a1ba12c2d46b95edf82c76ec1d394f29
skyerguo97 <skyerguo97@gmail.com>
2023-03-14 13:57:35 +0800

    更新文件结构

---
+++ igraph/igraph_shortest_path/build/CMakeFiles/igraph_shortest_path.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/igraph_shortest_path.dir/igraph_shortest_path.c.o"
+  "igraph_shortest_path"
+  "igraph_shortest_path.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/igraph_shortest_path.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
