0889cdeccc9663e114de4f7876f773a3f9dcd040
skyerguo97 <skyerguo97@gmail.com>
2023-03-15 11:21:15 +0800

    update uniform.py considering both directed and weighted

--- verified_dataset/uniform.py
+++ verified_dataset/uniform.py
@@ -8,16 +8,22 @@ import sys
 
 p = argparse.ArgumentParser(description='Analyze the result')
 p.add_argument('-i', '--input_file', type=str, default="", dest="inputfile", action="store", help="the name of input file")
-p.add_argument('-d', '--input_directory', type=str, default="", dest="inputdirectory", action="store", help="the name of input directory")
+p.add_argument('-r', '--input_directory', type=str, default="", dest="inputdirectory", action="store", help="the name of input directory")
 p.add_argument('-w', '--weighted', action="store_true", default=False, dest="weighted", help="wether a weighted graph")
+p.add_argument('-d', '--directed', action="store_true", default=False, dest="directed", help="wether a directed graph")
 
 args = p.parse_args()
 
 input_file = args.inputfile
-output1_weighted_file = args.inputdirectory + '/a.in'
-output1_unweighted_file = args.inputdirectory + '/a_unweighted.in'
-output2_weighted_file = args.inputdirectory + '/a.lgl'
-output2_unweighted_file = args.inputdirectory + '/a_unweighted.lgl'
+if args.weighted:
+    weight_item = "weighted"
+else:
+    weight_item = "unweighted"
+    
+if args.directed:
+    direct_item = "directed"
+else:
+    direct_item = "undirected"
 
 n = 0
 m = 0
@@ -27,8 +33,10 @@ if args.weighted:
 
 if __name__ == '__main__':
     f_in = open(input_file, 'r')
-    f_out1_weighted = open(output1_weighted_file, 'w')
-    f_out1_unweighted = open(output1_unweighted_file, 'w')
+    
+    # f_out1_weighted = open(output1_weighted_file, 'w')
+    # f_out1_unweighted = open(output1_unweighted_file, 'w')
+    f_out1 = open(args.inputdirectory + '/a_' + weight_item + '_' + direct_item + '.in', 'w')
     for line in f_in:
         n = max(n, max(int(re.split(r'[,\s]',line)[0]), int(re.split(r'[,\s]',line)[1])))
     f_in.close()
@@ -36,62 +44,47 @@ if __name__ == '__main__':
     
     edge_list_unweighted = [[] for _ in range(n)]
     
-
-    
     f_in = open(input_file, 'r')
     for line in f_in:
         u = int(re.split(r'[,\s]',line)[0])
         v = int(re.split(r'[,\s]',line)[1])
         
-        if u in edge_list_unweighted[v] or v in edge_list_unweighted[u] or u == v: ## 去除重边和自环
+        if v in edge_list_unweighted[u] or u == v: ## 去除重边和自环
+            continue
+        if (not args.directed) and u in edge_list_unweighted[v]: ## 无向图去除重边
             continue
         
         m += 1
 
         edge_list_unweighted[u].append(v)
-        edge_list_unweighted[v].append(u)
+        if not args.directed:
+            edge_list_unweighted[v].append(u)
         
         if args.weighted:
             w = random.randint(1, 100)
-            
             if u in edge_list_weighted.keys():
                 edge_list_weighted[u].append([v,w])
             else:
                 edge_list_weighted[u] = [[v,w]]
-                
-            
-    print(n, m, file=f_out1_unweighted)
-    if args.weighted:
-        print(n, m, file=f_out1_weighted)
+                    
+    print(n, m, file=f_out1)
     
-    
-    f_out2_unweighted = open(output2_unweighted_file, 'w')
-    if args.weighted:
-        f_out2_weighted = open(output2_weighted_file, 'w')
-        
+    f_out2 = open(args.inputdirectory + '/a_' + weight_item + '_' + direct_item + '.lgl', 'w')
     for i in range(n):
         if not edge_list_unweighted[i]:
             continue
-        print("# %i"%i, file=f_out2_unweighted)
+        print("# %i"%i, file=f_out2)
         
-        if args.weighted:
-            if i not in edge_list_weighted.keys():
-                continue    
-        
-        for j in edge_list_unweighted[i]:
-            print("%i"%(j), file=f_out2_unweighted)
-            print(i, j, file=f_out1_unweighted)
-        
-        if args.weighted:
-            for j in range(len(edge_list_weighted[i])):
-                print("%i %i %i"%(i, edge_list_weighted[i][j][0], edge_list_weighted[i][j][1]), file=f_out1_weighted)
-                print("%i %i"%(edge_list_weighted[i][j][0], edge_list_weighted[i][j][1]), file=f_out2_weighted)
-            
+        if not args.weighted:
+            for j in edge_list_unweighted[i]:
+                print(i, j, file=f_out1)
+                print("%i"%(j), file=f_out2)
+        else:
+            if args.weighted:
+                for j in range(len(edge_list_weighted[i])):
+                    print("%i %i %i"%(i, edge_list_weighted[i][j][0], edge_list_weighted[i][j][1]), file=f_out1)
+                    print("%i %i"%(edge_list_weighted[i][j][0], edge_list_weighted[i][j][1]), file=f_out2)
+                
     f_in.close()
-    f_out1_unweighted.close()
-    if args.weighted:
-        f_out1_weighted.close()
-            
-    f_out2_unweighted.close()
-    if args.weighted:
-        f_out2_weighted.close()
+    f_out1.close()
+    f_out2.close()
