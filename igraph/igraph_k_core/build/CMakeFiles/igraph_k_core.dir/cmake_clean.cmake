48bffd69890b5ad52240b55adbc44de997143df7
skyerguo97 <skyerguo97@gmail.com>
2023-03-16 17:15:43 +0800

    recover unawared deleting

---
+++ igraph/igraph_k_core/build/CMakeFiles/igraph_k_core.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/igraph_k_core.dir/igraph_k_core.c.o"
+  "igraph_k_core"
+  "igraph_k_core.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/igraph_k_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
